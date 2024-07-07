2f5ed52b39c30223e9419aeb3f1d3f9ca9b71629
dani-run <danimiinea@gmail.com>
2024-07-07 14:30:13 +0300

    i swear it works perfectly now

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,28 @@ import {
   json,
   useLoaderData,
   useLocation,
+  Links,
 } from "@remix-run/react";
 import { getSession } from "./utils/session.server";
 import { db } from "./utils/db.server";
 import { Layout } from "./components/layout";
 import {config} from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss= false;
-
+import { LinksFunction } from "@remix-run/node";
+import '/app/styles/output.css';
+import '/node_modules/@fortawesome/fontawesome-svg-core/styles.css';
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+    },
+    {
+      rel: "stylesheet",
+      href: 'https://fonts.googleapis.com/css2?family=Pacifico&display=swap'
+    },   
+  ];
+}
 
 export const loader = async ({request} : ClientLoaderFunctionArgs ) => {
   const session=await getSession (
@@ -38,10 +53,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Vasonim Forum</title>
-        <link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-svg-core/styles.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" />
-        <link rel="stylesheet" href="/app/styles/output.css" />
+        <Links />
         <script src="https://kit.fontawesome.com/1832baa3d3.js" crossOrigin="anonymous"></script>
       </head>
       <body>
